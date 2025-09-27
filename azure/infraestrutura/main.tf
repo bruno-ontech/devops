785c5c7e55cb2d7c8b956de97eb8b77df33c5ac6
Bruno Marques <oning.digital@gmail.com>
2025-09-27 15:11:01 -0300

    feat: adiciona CI/CD

--- azure/infraestrutura/main.tf
+++ azure/infraestrutura/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "main" {
 
 # Crie um aplicativo da web simples
 resource "azurerm_container_group" "webapp" {
-  name                = "Meu  WebApp"
+  name                = "Meu-WebApp"
   location           = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   ip_address_type     = "Public"
